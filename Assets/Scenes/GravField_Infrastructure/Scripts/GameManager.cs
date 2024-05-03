683c8c5a59774b4c2b35a1ba646336eae4d6548b
agalloch21 <agalloch21@gmail.com>
2024-05-03 23:06:01 +0800

    start building network infrastructure

---
+++ Assets/Scenes/GravField_Infrastructure/Scripts/GameManager.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using HoloKit.ImageTrackingRelocalization;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class GameManager : MonoBehaviour
+{
+    string performerPassword = "111";
+    public string PerformerPassword { get => performerPassword; set => performerPassword = value; }
+
+    string serverIp = "192.168.0.135";
+    public string ServerIp { get => serverIp; set => serverIp = value; }
+
+    private ImageTrackingStablizer relocalizationStablizer;
+    public ImageTrackingStablizer RelocalizationStablizer{ get=> relocalizationStablizer; }
+
+    void Start()
+    {
+        // Initialize References
+        relocalizationStablizer = FindObjectOfType<ImageTrackingStablizer>();
+        if (relocalizationStablizer == null)
+        {
+            Debug.LogError("No ImageTrackingStablizer Found.");
+        }
+
+
+        // Read Command Line
+        string mode_from_command = GetModeFromCommandLine();
+        switch (mode_from_command)
+        {
+            case "server":
+                JoinAsServer();
+                break;
+            case "performer":
+                JoinAsPerformer();
+                break;
+            case "client":
+                JoinAsAudience();
+                break;
+            default:
+                // Do nothing
+                break;
+        }
+    }
+
+    public void JoinAsServer()
+    {
+        OnBeforeHostStarted();
+
+        NetworkManager.Singleton.StartServer();
+    }
+
+    public void JoinAsPerformer()
+    {
+        OnBeforeClientStarted();
+
+        NetworkManager.Singleton.StartClient();
+
+        // Try to register as a performer
+    }
+
+    public void JoinAsAudience()
+    {
+        OnBeforeClientStarted();
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+
+    
+
+    #region Network
+    void OnBeforeHostStarted()
+    {
+        var unityTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        if (unityTransport != null)
+        {
+            string localIPAddress = GetLocalIPAddress();
+            unityTransport.SetConnectionData(localIPAddress, (ushort)7777);
+            unityTransport.ConnectionData.ServerListenAddress = "0.0.0.0";
+            //m_HostIPAddress.text = $"Host IP Address: {localIPAddress}";
+        }
+    }
+    void OnBeforeClientStarted()
+    {
+        var unityTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        if (unityTransport != null)
+        {
+            unityTransport.SetConnectionData(ServerIp, (ushort)7777);
+        }
+    }
+
+    string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+    #endregion
+
+    #region Instance
+    private static GameManager _Instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                _Instance = GameObject.FindObjectOfType<GameManager>();
+                if (_Instance == null)
+                {
+                    GameObject go = new GameObject();
+                    _Instance = go.AddComponent<GameManager>();
+                }
+            }
+            return _Instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _Instance = null;
+    }
+    #endregion
+
+    #region ARGS
+
+    private string GetModeFromCommandLine()
+    {
+        var args = GetCommandlineArgs();
+
+        if (args.TryGetValue("-mode", out string mode))
+        {
+            return mode;
+        }
+        return "";
+    }
+    private Dictionary<string, string> GetCommandlineArgs()
+    {
+        Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+        var args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length; ++i)
+        {
+            var arg = args[i].ToLower();
+            Debug.Log(arg);
+            if (arg.StartsWith("-"))
+            {
+                var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+                value = (value?.StartsWith("-") ?? false) ? null : value;
+
+                argDictionary.Add(arg, value);
+            }
+        }
+        return argDictionary;
+    }
+    #endregion
+}
---
+++ Assets/Scenes/GravField_Infrastructure/Scripts/UIController.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Text.RegularExpressions;
+using HoloKit.ImageTrackingRelocalization;
+
+public class UIController : MonoBehaviour
+{
+
+    Transform transButtonStart;
+    Transform transButtonPerformer;
+    Transform transButtonSettings;
+    Transform transPanelCalibration;
+    Transform transPanelPassword;
+    Transform transPanelServerIp;
+    Transform transPanelWarning;
+
+    void Start()
+    {
+        // Initialize
+        transButtonStart = transform.Find("Button_Start");
+        transButtonPerformer = transform.Find("Button_Performer");
+        transButtonSettings = transform.Find("Button_Settings");
+        transPanelCalibration = transform.Find("Panel_Calibration");
+        transPanelPassword = transform.Find("Panel_Password");
+        transPanelServerIp = transform.Find("Panel_ServerIp");
+        transPanelWarning = transform.Find("Panel_Warning");
+
+        if (transButtonStart == null || transButtonPerformer == null || transButtonSettings == null
+            || transPanelCalibration == null || transPanelPassword == null || transPanelServerIp == null
+            || transPanelWarning == null)
+        {
+            Debug.LogError("Can't find UI elements properly.");
+            return;
+        }
+
+        // Bind Basic Listener
+        transButtonStart.GetComponent<Button>().onClick.AddListener(()=>GotoPage(1, 0));
+        transButtonPerformer.GetComponent<Button>().onClick.AddListener(() => GotoPage(1, 1));
+        transButtonSettings.GetComponent<Button>().onClick.AddListener(() => GotoPage(1, 2));
+
+        transPanelCalibration.Find("Button_Close").GetComponent<Button>().onClick.AddListener(() => GotoPage(0));
+        transPanelPassword.Find("Button_Close").GetComponent<Button>().onClick.AddListener(() => GotoPage(0));
+        transPanelServerIp.Find("Button_Close").GetComponent<Button>().onClick.AddListener(() => GotoPage(0));
+
+        transPanelPassword.Find("Button_Enter").GetComponent<Button>().onClick.AddListener(OnEnterPassword);
+        transPanelServerIp.Find("Button_Enter").GetComponent<Button>().onClick.AddListener(OnEnterServerIp);
+
+        // Enter Home Page
+        GotoPage(0);
+    }
+
+    public void OnFinishRelocalization(Vector3 position, Quaternion rotation)
+    {
+        GameManager.Instance.RelocalizationStablizer.OnTrackedImagePoseStablized.RemoveListener(OnFinishRelocalization);
+
+        // Join As Audience
+        GameManager.Instance.JoinAsAudience();
+        GotoPage(2);
+    }
+
+    public void OnEnterPassword()
+    {
+        string password = transPanelPassword.Find("InputField_Password").GetComponent<TMP_InputField>().text;
+        
+
+        if(password == GameManager.Instance.PerformerPassword)
+        {
+            // Join As Performer
+            GameManager.Instance.JoinAsPerformer();
+            GotoPage(2);
+        }
+        else
+        {
+            // Show Error Info
+            ShowWarning("Wrong Password.");
+        }
+
+    }
+
+    public void OnEnterServerIp()
+    {
+        string server_ip = transPanelServerIp.Find("InputField_ServerIp").GetComponent<TMP_InputField>().text;
+
+        if(Regex.IsMatch(server_ip, @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"))
+        {
+            // Change Server Ip
+            GameManager.Instance.ServerIp = server_ip;
+            GotoPage(0);
+        }
+        else
+        {
+            // Show Error Info
+            ShowWarning("Wrong ServerIp.");
+        }
+    }
+
+    public void ShowWarning(string msg)
+    {
+        transPanelWarning.Find("Message").GetComponent<TextMeshProUGUI>().text = msg;
+        transPanelWarning.gameObject.SetActive(true);
+
+        StartCoroutine(HideWarning());
+    }
+
+    IEnumerator HideWarning()
+    {
+        yield return new WaitForSeconds(2);
+        transPanelWarning.gameObject.SetActive(false);
+    }
+
+
+    void GotoPage(int page_index, int panel_index = 0)
+    {
+        // Home Page
+        if(page_index == 0)
+        {
+            transButtonStart.gameObject.SetActive(true);
+            transButtonPerformer.gameObject.SetActive(true);
+            transButtonSettings.gameObject.SetActive(true);
+
+            transPanelCalibration.gameObject.SetActive(false);
+            transPanelPassword.gameObject.SetActive(false);
+            transPanelServerIp.gameObject.SetActive(false);
+
+            return;
+        }
+
+
+        // Calibration / Performer / ServerIp Page
+        if(page_index == 1)
+        {
+            transButtonStart.gameObject.SetActive(false);
+            transButtonPerformer.gameObject.SetActive(false);
+            transButtonSettings.gameObject.SetActive(false);
+            switch (panel_index)
+            {
+                case 0:
+                    transPanelCalibration.gameObject.SetActive(true);
+                    StartCalibration();
+                    break;
+                case 1:
+                    transPanelPassword.gameObject.SetActive(true);
+                    break;
+                case 2:
+                    transPanelServerIp.gameObject.SetActive(true);
+                    break;
+            }
+
+            return;
+        }
+
+
+        // In Game
+        if (page_index == 2)
+        {
+            transButtonStart.gameObject.SetActive(false);
+            transButtonPerformer.gameObject.SetActive(false);
+            transButtonSettings.gameObject.SetActive(false);
+
+            transPanelCalibration.gameObject.SetActive(false);
+            transPanelPassword.gameObject.SetActive(false);
+            transPanelServerIp.gameObject.SetActive(false);
+
+            return;
+        }
+    }
+
+
+    void StartCalibration()
+    {
+        ImageTrackingStablizer stablizer = GameManager.Instance.RelocalizationStablizer;
+        stablizer.OnTrackedImagePoseStablized.AddListener(OnFinishRelocalization);
+        stablizer.IsRelocalizing = true;
+    }
+}
