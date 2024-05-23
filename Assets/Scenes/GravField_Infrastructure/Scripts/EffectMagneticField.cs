5d5709c1d33d5ec0da1ebc6344cab6e56eb51b06
agalloch21 <agalloch21@gmail.com>
2024-05-23 18:44:07 +0800

    first version of MagneticEffect

--- Assets/Scenes/GravField_Infrastructure/Scripts/EffectMagneticField.cs
+++ Assets/Scenes/GravField_Infrastructure/Scripts/EffectMagneticField.cs
@@ -1,23 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EffectMagneticField : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //private bool isPerformingA = false;
+    //public bool IsPerformingA { get => isPerformingA; }
+
+    //private bool isPerformingB = false;
+    //public bool IsPerformingB { get => isPerformingB; }
+
+    //private bool isPerformingC = false;
+    //public bool IsPerformingC { get => isPerformingC; }
+
+    //private bool magneticA = true;
+    //public bool MagneticA { get => magneticA; }
+
+    //private bool magneticB = false;
+    //public bool MagneticB { get => magneticB; }
+
+    //private bool magneticC = true;
+    //public bool MagneticC { get => magneticC; }
+
+    public Transform performerTransformRoot;
+
+    List<Performer> performerList = new  List<Performer>();
+    VisualEffect vfx;
+    bool effectEnabled = false;
+
+    void Awake()
+    {
+        vfx = transform.GetComponentInChildren<VisualEffect>();
+
+        for(int i=0; i< performerTransformRoot.childCount; i++)
+        {
+            performerList.Add(performerTransformRoot.GetChild(i).GetComponent<Performer>());
+        }
+        
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (effectEnabled == false) return;
+
+        UpdateVFX();
     }
 
     public void SetEffectState(bool state)
     {
+        effectEnabled = state;
+        vfx.enabled = state;
+    }
 
+    void UpdateVFX()
+    {
+        vfx.SetBool("IsPerformingA", performerList[0].localData.isPerforming);
+        vfx.SetBool("IsPerformingB", performerList[1].localData.isPerforming);
+        vfx.SetBool("IsPerformingC", performerList[2].localData.isPerforming);
+
+        vfx.SetBool("MagneticA", performerList[0].localData.positive);
+        vfx.SetBool("MagneticB", performerList[1].localData.positive);
+        vfx.SetBool("MagneticC", performerList[2].localData.positive);
     }
 }
